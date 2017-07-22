d5422927b6d44334d672ae6e0b055b6e7d394b1b
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-22 10:56:49 -0300

    Create project skeletons

---
+++ apps/csv/lib/csv.ex
@@ -0,0 +1,18 @@
+defmodule Csv do
+  @moduledoc """
+  Documentation for Csv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Csv.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
