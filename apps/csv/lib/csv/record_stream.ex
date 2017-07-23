186c9ab74c3242621a6f4b32a9df4e28fc38b249
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-23 20:52:08 -0300

    Get RecordStream working

--- apps/csv/lib/csv/record_stream.ex
+++ apps/csv/lib/csv/record_stream.ex
@@ -1,6 +1,27 @@
 defmodule Csv.RecordStream do
-  def new(_device, headers: _headers, schema: _schema) do
-    stream = Stream.map([], fn(_) -> nil end)
-    {:ok, stream}
+  def new(device, headers: expected_headers, schema: schema) do
+    stream = device
+    |> IO.stream(:line)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, ","))
+
+    headers = stream
+    |> Enum.fetch!(0)
+    |> Enum.map(&String.to_atom/1)
+
+    if Enum.sort(headers) == Enum.sort(expected_headers) do
+      structs = stream
+      |> Stream.map(fn(row) ->
+        contents = headers
+        |> Enum.zip(row)
+        |> Enum.into(%{})
+
+        struct(schema, contents)
+      end)
+
+      {:ok, structs}
+    else
+      :invalid_csv
+    end
   end
 end
--- apps/csv/test/csv/record_stream_test.exs
+++ apps/csv/test/csv/record_stream_test.exs
@@ -1,10 +1,46 @@
 defmodule RecordStreamTest do
   use ExUnit.Case
+  alias Csv.Schemas.Site
 
   test "streams an empty list when csv has no rows" do
     {:ok, device} = StringIO.open("name,url\n")
     {:ok, stream} = Csv.RecordStream.new(device, headers: ~w(name url)a, schema: Site)
 
     assert [] == Enum.to_list(stream)
+  end
+
+  test "streams csv rows as structs" do
+    {:ok, device} = StringIO.open """
+    name,url
+    Elixir Language,https://elixir-lang.org
+    Semaphore Blog,https://semaphoreci.com/blog
+    """
+    {:ok, stream} = Csv.RecordStream.new(device, headers: ~w(name url)a, schema: Site)
+
+    assert [
+      %Site{name: "Elixir Language", url: "https://elixir-lang.org"},
+      %Site{name: "Semaphore Blog", url: "https://semaphoreci.com/blog"}
+    ] = Enum.to_list(stream)
+  end
+
+  test "streams correctly when headers are shuffled" do
+    {:ok, device} = StringIO.open """
+    url,name
+    https://elixir-lang.org,Elixir Language
+    """
+    {:ok, stream} = Csv.RecordStream.new(device, headers: ~w(name url)a, schema: Site)
+
+    assert [
+      %Site{name: "Elixir Language", url: "https://elixir-lang.org"},
+    ] = Enum.to_list(stream)
+  end
+
+  test "returns :invalid_csv when missing required columns" do
+    {:ok, device} = StringIO.open """
+    name
+    Elixir Language
+    """
+
+    assert :invalid_csv == Csv.RecordStream.new(device, headers: ~w(name url)a, schema: Site)
   end
 end
